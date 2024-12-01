caf6a673459b4fc87e4264a9fbfe2b469509c398
Ethan Hickman <ethickman03@gmail.com>
2024-11-30 20:01:28 -0800

    Update server.js

--- server.js
+++ server.js
@@ -7,44 +7,102 @@
  */
 var path = require('path');
 var express = require('express');
-var exphbs = require('express-handlebars');
+
 var app = express();
 var port = process.env.PORT || 3000;
-var photoData = require('./postData.json');
-var photoCard;
+const data = require("./twitData.json");
+const ViewEngine = require("express-handlebars");
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+function basicPages(req,res,next){
+	var pagenum = null;
+	if(req.url === "/"){
+		pagenum = 0;
+	}
+	else if(req.params.nav){
+		switch(req.params.nav.toLowerCase()){
+			case "home":
+				pagenum = 0;
+				break;
+			case "trending":
+				pagenum = 1;
+				break;
+			case "people":
+				pagenum = 2;
+				break;
+			case "mystery":
+				pagenum = 3;
+				break;
+			default:
+				pagenum = -1;
+				req["404"] = -1;
+				break;
+		}
+	}
+	else{
+		pagenum = -5;
+	}
+
+ 	const navs =  [{str: "Home"} , {str: "Trending"}, {str:"People"}, {str: "Mystery"}];
+	if(pagenum > -1){
+		navs[pagenum].active = true;
+	}
+	navs[3].active = true;
+	req.handlebarnav = navs;
+
+	next();
+	return;
+}
+
+app.engine('handlebars', ViewEngine());
 app.set('view engine', 'handlebars');
+app.get("/", basicPages, function(req,res,next){
 
+		res.status(200).render("index",{
+			twits: data,
+			navlinks: req.handlebarnav,
+			layout:false
+		})
+		return;
+});
+app.get('/:nav',basicPages, function(req,res,next){
+
+		if(req["404"] === -1){
+			next();
+			return;
+		}
+		res.status(200).render("index",{
+			twits: data,
+			navlinks: req.handlebarnav,
+			layout:false
+		})
+		return;
+})
+app.get('/twits/:index',(req,res,next)=>{
+	const ind = parseInt(req.params.index);
+	if(!ind){
+		next();
+	}
+	else if(ind > -1 && ind < data.length){
+		const navs =  [{str: "Home"} , {str: "Trending"}, {str:"People"}, {str: "Mystery",active: true}];
+		res.status(200).render("TwitDetail",{
+			twit: data[ind],
+			navlinks: navs,
+			layout:false
+		})
+	}
+	else{
+		next();
+	}
+})
 app.use(express.static('public'));
 
-app.get('', function (req, res)
-{
-  photoCard = photoData;
-  res.render('photoPages', {photoCard});
+app.get('*', basicPages, function (req, res) {
+  res.status(404).render('404',{
+		navlinks: req.handlebarnav,
+		layout:false
+	})
 });
 
-app.get('/:photoCardNum', function(req, res, next)
-{
-  var photoCardNum = req.params.photoCardNum;
-
-  if(photoCardNum > 0 && photoCardNum <= 8)
-  {
-    photoCard = [photoData[photoCardNum - 1]];
-    res.status(200).render('photoPages', {photoCard});
-  }
-  else
-  {
-    next();
-  }
-})
-
-app.get('*', function(req, res, next)
-{
-  res.status(404).render('404')
-});
-
-app.listen(port, function ()
-{
+app.listen(port, function () {
   console.log("== Server is listening on port", port);
 });
