1a408f89e591975861fa7f96112d3c441a8fe539
Ethan Hickman <ethickman03@gmail.com>
2024-11-30 21:59:17 -0800

    Update server.js

--- server.js
+++ server.js
@@ -8,43 +8,36 @@
 var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
+
+
 var app = express();
 var port = process.env.PORT || 3000;
-var photoData = require('./postData.json');
-var photoCard;
+var postData = require('./postData')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
+app.get('/', function(req, res) {
+  res.status(200).render('postPage', postData);
+});
+
+app.get('/posts/:postId', function(req, res, next) {
+    var postId = parseInt(req.params.postId);
+
+    if(postId >= 0 && postId < postData.length){
+      res.status(200).render('postPage', postData[postId]);
+    } else {
+      next();
+    }
+});
+
 app.use(express.static('public'));
 
-app.get('', function (req, res)
-{
-  photoCard = photoData;
-  res.render('photoPages', {photoCard});
+app.get('*', function (req, res) {
+  res.status(404).render('404');
+
 });
 
-app.get('/:photoCardNum', function(req, res, next)
-{
-  var photoCardNum = req.params.photoCardNum;
-
-  if(photoCardNum > 0 && photoCardNum <= 8)
-  {
-    photoCard = [photoData[photoCardNum - 1]];
-    res.status(200).render('photoPages', {photoCard});
-  }
-  else
-  {
-    next();
-  }
-})
-
-app.get('*', function(req, res, next)
-{
-  res.status(404).render('404')
-});
-
-app.listen(port, function ()
-{
+app.listen(port, function () {
   console.log("== Server is listening on port", port);
 });
